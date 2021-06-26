cb9d75a66ac21d6de135d843cfa855c426fbee31
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-26 08:55:08 +0100

    create nav link

--- src/components/layout/SignedInLink.js
+++ src/components/layout/SignedInLink.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export const SignedInLink = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/create'>Create New Project</NavLink></li>
+            <li><NavLink to='/project/create'>Create New Project</NavLink></li>
             <li><NavLink to='/auth/logout'>Logout</NavLink></li>
             <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
         </ul>
